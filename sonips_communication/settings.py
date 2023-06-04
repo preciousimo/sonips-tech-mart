fd2870f7ff80703305a181dd65c7af16816c4af0
preciousimo <preciousimoniakemu@gmail.com>
2023-06-04 03:05:54 +0100

    Custom Django Admin using Jet-Admin

--- sonips_communication/settings.py
+++ sonips_communication/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'jet.dashboard',
     'jet',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -141,6 +141,41 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Django Jet Admin
+JET_THEMES = [
+    {
+        'theme': 'default', # theme folder name
+        'color': 'blue', # color of the theme's button in user menu
+        'title': 'Default' # theme title
+    },
+    {
+        'theme': 'green',
+        'color': '#44b78b',
+        'title': 'Green'
+    },
+    {
+        'theme': 'light-green',
+        'color': '#2faa60',
+        'title': 'Light Green'
+    },
+    {
+        'theme': 'light-violet',
+        'color': '#a464c4',
+        'title': 'Light Violet'
+    },
+    {
+        'theme': 'light-blue',
+        'color': '#5EADDE',
+        'title': 'Light Blue'
+    },
+    {
+        'theme': 'light-gray',
+        'color': '#222',
+        'title': 'Light Gray'
+    },
+]
+
+JET_SIDE_MENU_COMPACT = True
 
 # EMAIL - SMTP SERVER (from google gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
