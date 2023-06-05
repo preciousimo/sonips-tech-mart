643961c325d6ead73e5bdc2f1896a859aeaf04c9
preciousimo <preciousimoniakemu@gmail.com>
2023-06-05 08:19:29 +0100

    Login Users

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import User
 
 def signup(request):
     if request.user.is_authenticated:
@@ -19,3 +20,28 @@ def signup(request):
         form =  UserRegisterForm()
     context = {'form': form}
     return render(request, "userauths/signup.html", context)
+
+def login(request):
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already logged in.")
+        return redirect('/')
+    
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome {email}. You are now logged in.")
+                return redirect('/')
+            else:
+                messages.warning(request, "User does not exist, create an account.")
+                
+        except:
+            messages.warning(request, f'User with {email} does not exist')
+
+    return render(request, 'userauths/login.html')
