6069bcfa9560167177bb30f60d55b1f1346c89a2
preciousimo <preciousimoniakemu@gmail.com>
2023-06-06 14:10:48 +0100

    Category list

--- product/views.py
+++ product/views.py
@@ -3,10 +3,25 @@ from django.shortcuts import render
 
 from product.models import Product, Category
 
-def product_list(request):
-    products = Product.objects.all()
+import random
 
+def product_list(request):
+    category_id = request.GET.get('category')
+    categories = Category.objects.all()
+    active_category = None
+    
+    if category_id:
+        products = Product.objects.filter(category__id=category_id)
+        active_category = Category.objects.get(id=category_id)
+    else:
+        products = list(Product.objects.all())
+        total_products = len(products)
+        num_products = min(total_products, 6)
+        products = random.sample(products, num_products)
+    
     context = {
-        "products":products
+        'products': products,
+        'categories': categories,
+        'active_category': active_category,
     }
     return render(request, 'product/product-list.html', context)
