1c2e570e0b22a786f8505a19831b9698530bb795
preciousimo <preciousimoniakemu@gmail.com>
2023-08-10 18:40:55 +0100

    Fixes

--- sonips_communication/settings.py
+++ sonips_communication/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", ".now.sh", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
