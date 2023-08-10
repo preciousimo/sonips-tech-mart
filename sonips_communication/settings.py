2708083235dcb9e623a7ae7a97b4b138718ec90d
preciousimo <preciousimoniakemu@gmail.com>
2023-08-10 17:34:26 +0100

    Fixes

--- sonips_communication/settings.py
+++ sonips_communication/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,39 +1,41 @@
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from product.models import Product, Wishlist
 
-# Create your views here.
 def wishlist(request):
-    user = request.user
-    wishlist_items = Wishlist.objects.filter(user=user)
-    context = {
-        'wishlist_items': wishlist_items
-    }
-    return render(request, 'wishlist/wishlist.html', context)
+    if request.user.is_authenticated:
+        user = request.user
+        wishlist_items = Wishlist.objects.filter(user=user)
+        context = {
+            'wishlist_items': wishlist_items
+        }
+        return render(request, 'wishlist/wishlist.html', context)
+    else:
+        messages.error(request, 'Please log in to view your wishlist.')
+        return redirect('login')
 
 def add_to_wishlist(request, product_id):
     if request.user.is_authenticated:
         user = request.user
         product = Product.objects.get(id=product_id)
         wishlist_item = Wishlist.objects.create(user=user, product=product)
-        # You can add any additional logic here (e.g., showing a success message)
+        messages.success(request, 'Product added to wishlist successfully.')
         return redirect('wishlist')
     else:
-        # Handle the case where the user is not authenticated
+        messages.error(request, 'Please log in to add items to your wishlist.')
         return redirect('login')
 
 def remove_from_wishlist(request, item_id):
-    # Retrieve the Wishlist object using the item_id
-    try:
-        wishlist_item = Wishlist.objects.get(id=item_id)
-    except Wishlist.DoesNotExist:
-        # Handle the case when the wishlist item is not found
-        # Redirect or show an error message
-        return HttpResponse("Wishlist item does not exist.")
-    
-    # Delete the wishlist item
-    wishlist_item.delete()
-
-    # Redirect to the wishlist page or any other desired location
-    return redirect('wishlist') 
+    if request.user.is_authenticated:
+        try:
+            wishlist_item = Wishlist.objects.get(id=item_id, user=request.user)
+            wishlist_item.delete()
+            messages.success(request, 'Product removed from wishlist successfully.')
+        except Wishlist.DoesNotExist:
+            messages.error(request, 'Wishlist item does not exist.')
+        return redirect('wishlist')
+    else:
+        messages.error(request, 'Please log in to remove items from your wishlist.')
+        return redirect('login')
